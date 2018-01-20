c8147a1a4c018a46443d5e1bfd40a847d6efad6d
jaf7 <janthonyfields@gmail.com>
2018-01-19 23:21:29 -0500

    Monkey patched Hash using #select & #map

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,23 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+
+  def keys_of(*arguments)
+    # binding.pry
+    self.select {|key, value| arguments.include?(value)}.map {|k, v| k}
   end
+
+  # The method below works, but has code smell?
+  # https://stackoverflow.com/a/13485482
+
+  # def keys_of(*arguments)
+  #   key_array = self.collect do |key, value|
+  #     # binding.pry
+  #     if arguments.include?(value)
+  #       key
+  #     end
+  #   end
+  #   # binding.pry
+  #   key_array.compact
+  # end
+
 end
